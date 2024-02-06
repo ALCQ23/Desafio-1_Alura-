f440d8c5794e49c8469c60e074be0f8d30eb9b85
Andrea Cabrera <103345691+ALCQ23@users.noreply.github.com>
2024-02-05 21:49:29 -0600

    desafio 1

---
+++ desafio1/app.js
@@ -0,0 +1,19 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML ='Hola del Desafio';
+
+function mostarMensajeEnConsola()  {
+    console.log('El boton fue clicado!')
+}
+function mostrarAlerta(){
+    let cuidad = prompt('Por favor,ingrese el nombre de una cuidad de brasil:');
+    alert('Estuve en '+ ciudad +" y me acorde de ti.");
+
+}function mostrarAlerta(){
+    alert("AMO JS");
+}
+function sumaDosNumeros(){
+    let primerNumero = parseInt(prompt('Digite el primer numero'));
+    let segundoNumero = parseInt(prompt('Digite el segundo numero'));
+    let resultado = primerNumero + segundoNumero;
+    alert('${primerNumero} + ${sugundoNumero} = ${reaultado}')
+}
